c9ff7a1fbcb96bd616275c7234e38ee88f3124dc
megychen <megy.chen203@gmail.com>
2018-08-13 16:07:17 +0800

    Implement backend home page

---
+++ src/pages/backend/Backend.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+    <backend-header></backend-header>
+    <backend-content></backend-content>
+  </div>
+</template>
+
+<script>
+import BackendHeader from './components/Header'
+import BackendContent from './components/Content'
+export default {
+  name: 'Backend',
+  components: {
+    BackendHeader,
+    BackendContent
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    width: 1024px
+    margin: 20px auto
+    border: 1px solid #eaeaea
+    padding: 20px
+</style>
---
+++ src/pages/backend/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    后台管理
+    <router-link to="/"><span class="back">返回首页</span></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackendHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    color: #dc5a3b
+    font-size: 28px
+    text-align: center
+    margin-bottom: 30px
+    .back
+      font-size: 16px
+      color: #666
+      float: right
+</style>
